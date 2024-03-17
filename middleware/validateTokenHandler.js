fde76fbbfbdd7e7a9b248277d197375739cf4993
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-03-17 14:02:17 +0800

    Fix token validation and error handling in validateTokenHandler.js

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -8,20 +8,23 @@ const validateToken = asyncHandler(async (req, res, next) => {
   if (authHeader && authHeader.startsWith("Bearer")) {
     token = authHeader.split(" ")[1];
 
+    if (!token) {
+      res.status(401);
+      throw new Error("User is not authorized | Token not found");
+    }
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
         res.status(401);
         throw new Error("User is not authorized");
       }
-    //   console.log(decoded);
-    req.user = decoded.user;
-    next();
-    });
 
-    if(!token){
-        res.status(401);
-        throw new Error("User is not authorized | Token not found");
-    }
+      req.user = decoded.user;
+      next();
+    });
+  } else {
+    res.status(401);
+    throw new Error("User is not authorized | Authorization header not found");
   }
 });
 
