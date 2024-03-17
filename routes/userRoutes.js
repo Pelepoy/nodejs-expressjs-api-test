7183135d86d376e39975b75ce76189d55bf2eb35
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-03-17 14:02:39 +0800

    Refactor userRoutes.js to improve code readability and organization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, getUser, updateUser, deleteUser } = require("../controllers/userController");
 const validateToken = require("../middleware/validateTokenHandler");
 const admin = require("../middleware/adminValidation");
+const {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
 
 
 router.route("/").get(validateToken, admin, getUsers);
-
-router.route("/:id").get(validateToken, getUser)
-.put(validateToken, updateUser)
-
-router.delete("/:id", validateToken, deleteUser );
-
-
-// router.get("/profile", validateToken, currentUser );
+router
+  .route("/:id")
+  .get(validateToken, getUser)
+  .put(validateToken, updateUser)
+  .delete(validateToken, deleteUser);
 
 module.exports = router;
